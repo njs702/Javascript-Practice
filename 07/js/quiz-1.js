1fd701bccee45695645226361c0621fad2255008
junseok <njs702@ajou.ac.kr>
2021-09-27 14:28:02 +0900

    자바스크립트 7장완료

--- 07/js/quiz-1.js
+++ 07/js/quiz-1.js
@@ -1,4 +1,12 @@
 		var numbers = [2, 4, 6, 8, 10];
+		var sum = 0;
+		showArray(numbers);
+		for(var i=0;i<numbers.length;i++){
+			sum += numbers[i];
+		}
+
+		numbers.push(sum);
+
 		showArray(numbers);
 
 		function showArray(arr) {
@@ -9,3 +17,4 @@
 			str += "</tr></table>";
 			document.write(str);
 		}
+
