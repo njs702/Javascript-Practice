0bb2b1c42020d0bb2a9e3e386a8c75ebb3c05040
junseok <njs702@ajou.ac.kr>
2021-09-29 15:32:27 +0900

    register 이전까지 완료

--- 08/js/product.js
+++ 08/js/product.js
@@ -1,5 +1,20 @@
 		var bigPic = document.querySelector("#cup");  
-		var smallPics = document.querySelectorAll(".small");  
+		var smallPics = document.querySelectorAll(".small");
+		var view = document.querySelector("#view");
+		var isOpen = false;
+		
+		view.addEventListener("click",function(){
+			if(isOpen == false){
+				document.querySelector("#detail").style.display = "block";
+				view.innerText = "상세 설명 닫기";
+				isOpen = true;
+			}
+			else{
+				document.querySelector("#detail").style.display = "none";
+				view.innerText = "상세 설명 보기";
+				isOpen = false;
+			}
+		});
 
 		for(i=0; i<smallPics.length; i++) {
 			smallPics[i].addEventListener("click", function() {
